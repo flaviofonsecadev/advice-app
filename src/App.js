ecab300e6da8f7f19943c25c72fa7cf8cd088f11
flaviofonsecadev <flaviofonsecadev@gmail.com>
2022-01-14 21:06:03 -0300

    fetchAdvice ok

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends React.Component {
+
+  state = {
+    advice: ''
+  }
+
+  componentDidMount () {
+    this.fetchAdvice()
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+        .then((response) => {
+          const { advice } = response.data.slip
+          
+          this.setState({ advice });
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+
+  }
+
+  render () {
+    const { advice } = this.state;
+
+
+    return (
+      <h1>{advice}</h1>
+    );
+  }
+  
+
+
+
+}
+
+export default App;
